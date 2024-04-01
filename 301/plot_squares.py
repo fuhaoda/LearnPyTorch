0c28517c2f6ce0dff96cb2fbbf20a66b7f80d024
Haoda Fu <fu_haoda@lilly.com>
2024-03-31 22:48:13 -0400

    complete seq2seq model

--- 301/plot_squares.py
+++ 301/plot_squares.py
@@ -43,7 +43,7 @@ def counter_vs_clock(basic_corners=None, basic_colors=None, basic_letters=None, 
     if basic_letters is None:
         basic_letters = ['A', 'B', 'C', 'D']        
     
-    fig, axs = plt.subplots(1, 1 + draw_arrows, figsize=(3 + 3 * draw_arrows, 3))
+    fig, axs = plt.subplots(1 + draw_arrows, 1, figsize=(3, 3+ 3 * draw_arrows))
     if not draw_arrows:
         axs = [axs]
 
@@ -104,6 +104,38 @@ def counter_vs_clock(basic_corners=None, basic_colors=None, basic_letters=None, 
     
     return fig
 
+def sequence_pred(sbs_obj, X, directions=None, n_rows=2, n_cols=5):
+    fig, axs = plt.subplots(n_rows, n_cols, figsize=(4*n_cols, 4*n_rows))
+    axs = axs.flatten()
+
+    for e, ax in enumerate(axs):
+        first_corners = X[e, :2, :]
+        sbs_obj.model.eval()
+        next_corners = sbs_obj.model(X[e:e+1, :2].to(sbs_obj.device)).squeeze().detach().cpu().numpy()
+        pred_corners = np.concatenate([first_corners, next_corners], axis=0)
+
+        for j, corners in enumerate([X[e], pred_corners]):
+            for i in range(4):
+                coords = corners[i]
+                color = 'k'
+                ax.scatter(*coords, c=color, s=400)
+                if i == 3:
+                    start = -1
+                else:
+                    start = i
+                if (not j) or (j and i):
+                    ax.plot(*corners[[start, start+1]].T, c='k', lw=2, alpha=.5, linestyle='--' if j else '-')
+                ax.text(*(coords - np.array([.04, 0.04])), str(i+1), c='w', fontsize=12)
+                if directions is not None:
+                    ax.set_title(f'{"Counter-" if not directions[e] else ""}Clockwise')
+
+        ax.set_xlabel(r"$x_0$")
+        ax.set_ylabel(r"$x_1$", rotation=0)
+        ax.set_xlim([-1.7, 1.7])
+        ax.set_ylim([-1.7, 1.7])        
+
+    fig.tight_layout()
+    return fig
 
 
 def add_arrow(line, position=None, direction='right', size=15, color=None, lw=2, alpha=1.0, text=None, text_offset=(0 , 0)):
@@ -143,6 +175,8 @@ def add_arrow(line, position=None, direction='right', size=15, color=None, lw=2,
             xy=(xdata[end_ind], ydata[end_ind]),
             size=size,
         )
+
+
 
 # plot the clock and counter-clock sequence with the unknowns gray out
 counter_vs_clock(binary=False)
---
+++ 301/seq2seq_simple.py
@@ -0,0 +1,139 @@
+import torch
+import torch.optim as optim
+import torch.nn as nn
+from torch.utils.data import DataLoader, Dataset, random_split, TensorDataset
+from square_data_generation import generate_sequences
+from model_trainer import ModelTrainer
+from plot_squares import sequence_pred
+
+class Encoder(nn.Module):
+    def __init__(self, n_features, hidden_dim):
+        super().__init__()
+        self.hidden_dim = hidden_dim
+        self.n_features = n_features
+        self.hidden = None
+        self.basic_rnn = nn.GRU(self.n_features, self.hidden_dim, batch_first=True)               
+    def forward(self, X):        
+        rnn_out, self.hidden = self.basic_rnn(X)
+        return rnn_out # N, L, F
+    
+class Decoder(nn.Module):
+    def __init__(self, n_features, hidden_dim):
+        super().__init__()
+        self.hidden_dim = hidden_dim
+        self.n_features = n_features
+        self.hidden = None
+        self.basic_rnn = nn.GRU(self.n_features, self.hidden_dim, batch_first=True) 
+        self.regression = nn.Linear(self.hidden_dim, self.n_features)
+        
+    def init_hidden(self, hidden_seq):
+        # We only need the final hidden state
+        hidden_final = hidden_seq[:, -1:] # N, 1, H
+        # But we need to make it sequence-first
+        self.hidden = hidden_final.permute(1, 0, 2) # 1, N, H                      
+        
+    def forward(self, X):
+        # X is N, 1, F
+        batch_first_output, self.hidden = self.basic_rnn(X, self.hidden) 
+        
+        last_output = batch_first_output[:, -1:]
+        out = self.regression(last_output)
+        
+        # N, 1, F
+        return out.view(-1, 1, self.n_features)  
+    
+
+class EncoderDecoder(nn.Module):
+    def __init__(self, encoder, decoder, input_len, target_len, teacher_forcing_prob=0.5):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.input_len = input_len
+        self.target_len = target_len
+        self.teacher_forcing_prob = teacher_forcing_prob
+        self.outputs = None
+        
+    def init_outputs(self, batch_size):
+        device = next(self.parameters()).device
+        # N, L (target), F
+        self.outputs = torch.zeros(batch_size, 
+                              self.target_len, 
+                              self.encoder.n_features).to(device)
+    
+    def store_output(self, i, out):
+        # Stores the output
+        self.outputs[:, i:i+1, :] = out
+        
+    def forward(self, X):               
+        # splits the data in source and target sequences
+        # the target seq will be empty in testing mode
+        # N, L, F
+        source_seq = X[:, :self.input_len, :]
+        target_seq = X[:, self.input_len:, :]
+        self.init_outputs(X.shape[0])        
+        
+        # Encoder expected N, L, F
+        hidden_seq = self.encoder(source_seq)
+        # Output is N, L, H
+        self.decoder.init_hidden(hidden_seq)
+        
+        # The last input of the encoder is also
+        # the first input of the decoder
+        dec_inputs = source_seq[:, -1:, :]
+        
+        # Generates as many outputs as the target length
+        for i in range(self.target_len):
+            # Output of decoder is N, 1, F
+            out = self.decoder(dec_inputs)
+            self.store_output(i, out)
+            
+            prob = self.teacher_forcing_prob
+            # In evaluation/test the target sequence is
+            # unknown, so we cannot use teacher forcing
+            if not self.training:
+                prob = 0
+                
+            # If it is teacher forcing
+            if torch.rand(1) <= prob:
+                # Takes the actual element
+                dec_inputs = target_seq[:, i:i+1, :]
+            else:
+                # Otherwise uses the last predicted output
+                dec_inputs = out
+            
+        return self.outputs
+
+# Setup the model
+torch.manual_seed(123)
+encoder = Encoder(n_features=2, hidden_dim=2)
+decoder = Decoder(n_features=2, hidden_dim=2)
+model = EncoderDecoder(encoder, decoder, input_len=2, target_len=2, teacher_forcing_prob=0.5)
+loss = nn.MSELoss()
+optimizer = optim.Adam(model.parameters(), lr=0.01)
+
+# Training Data
+points, directions = generate_sequences(n=256)
+full_train = torch.as_tensor(points).float()
+target_train = full_train[:, 2:]
+
+# Testing Data
+test_points, test_directions = generate_sequences(n=1024)
+full_test = torch.as_tensor(test_points).float()
+source_test = full_test[:, :2]
+target_test = full_test[:, 2:]
+
+# Dataset and DataLoaders
+train_data = TensorDataset(full_train, target_train)
+test_data = TensorDataset(source_test, target_test)
+
+train_loader = DataLoader(train_data, batch_size=16, shuffle=True)
+test_loader = DataLoader(test_data, batch_size=16)
+
+# Train the model
+seq2seq_simple = ModelTrainer(model, loss, optimizer)
+seq2seq_simple.set_loaders(train_loader, test_loader)
+seq2seq_simple.train(100)
+
+seq2seq_simple.plot_losses()
+
+sequence_pred(seq2seq_simple, full_test, test_directions)
